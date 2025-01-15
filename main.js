4c7df26af6e229f6b28dd626019058430654500f
SolDoubt42 <sleepbloodmoon+sol@gmail.com>
2025-01-14 20:13:41 -0500

    333

--- main.js
+++ main.js
@@ -12,12 +12,10 @@ const showMenu = (toggleId, navId) => {
 showMenu('nav-toggle', 'nav-menu');
 
 /* GSAP Animation */
-
 gsap.from('.home-title', {opacity: 0, duration: 1.5, delay: .5, y: 50});
 gsap.from('.home-description', {opacity: 0, duration: 1.5, delay: .8, y: 50});
 
-gsap.from('.airpod1', {opacity: 0, duration: 2, delay: 1.2, y: 40});
-gsap.from('.airpod2', {opacity: 0, duration: 2, delay: 1.4, y: 40});
+gsap.from('.airpod', {opacity: 0, duration: 2, delay: 1.2, y: 40});
 
 gsap.from('.nav-logo', {opacity: 0, duration: 2.5, delay: 1.5, y: 25});
 gsap.from('.nav-toggle', {opacity: 0, duration: 2.5, delay: 1.7, y: 25});
@@ -33,11 +31,9 @@ animate
 .from('.details-img', {opacity: 0, y: 25})
 .from('.details-box', {opacity: 0, y: 25, duration: 1, ease:'expo.out', stagger: .4})
 
-
 var scene = new ScrollMagic.Scene({
     triggerElement: ".details-box"
 })
-
 .setTween(animate)
 .addTo(controller)
 
